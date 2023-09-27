d1ee38e82671ca9966ad448ce7312e337df491d8
João Paulo de Oliveira <jpos.usp@gmail.com>
2023-09-27 15:14:04 -0300

    cria decodificador de instrucao

---
+++ src/processo/DecodificadorDeInstrucao.java
@@ -0,0 +1,40 @@
+package processo;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DecodificadorDeInstrucao {
+    private final int tempoDeEspera;
+    private Pattern atribuicaoPattern = Pattern.compile("(?:X|Y)=([0-9]+)");
+    private Pattern ESPattern = Pattern.compile("^E\\\\/S$");
+
+    public DecodificadorDeInstrucao(int tempoDeEspera) {
+        this.tempoDeEspera = tempoDeEspera;
+    }
+
+    public void decodifica(BCP processo, String instrucao) {
+        Matcher matcherAtribuicao = atribuicaoPattern.matcher(instrucao);
+        Matcher matcherES = ESPattern.matcher(instrucao);
+
+        if (matcherAtribuicao.matches()) {
+            fazAtribuicao(processo, matcherAtribuicao);
+        } else if (matcherES.matches()) {
+            processo.setTempoDeEspera(tempoDeEspera);
+            processo.setEstadoProcesso(EstadoProcesso.BLOQUEADO);
+        } else {
+
+        }
+    }
+
+    private void fazAtribuicao(BCP processo, Matcher matcher) {
+        String registro = matcher.group(1);
+        int valor = Integer.parseInt(matcher.group(2));
+
+        if ("X".equals(registro)) {
+            processo.setX(valor);
+        } else {
+            processo.setY(valor);
+        }
+        processo.setEstadoProcesso(EstadoProcesso.PRONTO);
+    }
+}
