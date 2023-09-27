193f40cdbac1485cb8607ee846b1de587542a5fc
João Paulo de Oliveira <jpos.usp@gmail.com>
2023-09-27 15:13:23 -0300

    adiciona nova feature ao escalonador de verificar bloqueados

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 import escalonador.Escalonador;
-import processo.BCP;
-
-import java.util.ArrayList;
+import processo.DecodificadorDeInstrucao;
+import repositorio.RepositorioDeProcessosImpl;
 
 public class Main {
+    private static final int TEMPO_ESPERA = 2;
+    private static final int QUANTUM = 2;
     public static void main(String[] args) {
-        new Escalonador(new ArrayList<BCP>());
+        Escalonador escalonador = new Escalonador(
+                new RepositorioDeProcessosImpl(),
+                new DecodificadorDeInstrucao(TEMPO_ESPERA),
+                QUANTUM
+        );
+
+        escalonador.inicia();
     }
 }
--- src/escalonador/Escalonador.java
+++ src/escalonador/Escalonador.java
@@ -1,26 +1,91 @@
 package escalonador;
 
 import processo.BCP;
-import processo.EstadoProcesso;
+import processo.DecodificadorDeInstrucao;
+import repositorio.RepositorioDeProcessos;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
-import static processo.EstadoProcesso.*;
+import static processo.EstadoProcesso.BLOQUEADO;
+import static processo.EstadoProcesso.EXECUTANDO;
 
 public class Escalonador {
-    private List<BCP> processos;
-    private List<BCP> processosBloqueados = new ArrayList<>();
+    private static final String SAIDA = "SAIDA";
+    private List<BCP> tabelaDeProcessos;
+    private final DecodificadorDeInstrucao decodificador;
+    private final int quantum;
+    private Queue<BCP> processosProntos;
+    private Queue<BCP> processosBloqueados;
+    private int numProcessosExecutados = 0;
 
-    public Escalonador(ArrayList<BCP> processos) {
-        this.processos = processos;
+    public Escalonador(
+            RepositorioDeProcessos repositorio,
+            DecodificadorDeInstrucao decodificador,
+            int quantum
+    ) {
+        this.decodificador = decodificador;
+        this.quantum = quantum;
+        processosProntos = new PriorityQueue<>();
+        tabelaDeProcessos = repositorio.getProcessos();
+        if (tabelaDeProcessos != null) {
+            processosProntos.addAll(tabelaDeProcessos);
+        }
+        processosBloqueados = new PriorityQueue<>();
     }
 
-    public void addProcesso(BCP processo) {
-        if (processo.getEstadoProcesso() == BLOQUEADO) {
-            processosBloqueados.add(processo);
-        } else {
-            processos.add(processo);
+    public void inicia() {
+        while (!(processosProntos.isEmpty() && processosBloqueados.isEmpty())) {
+            BCP processo = processosProntos.poll();
+            if (processo == null) {
+                verificaBloqueados();
+            } else {
+                processaPronto(processo);
+            }
+        }
+    }
+
+    private void processaPronto(BCP processo) {
+        processo.setEstadoProcesso(EXECUTANDO);
+        numProcessosExecutados++;
+        int localQuantum = quantum;
+        while (localQuantum > 0) {
+            String instrucao = processo.getInstrucao();
+            if (instrucao.equals(SAIDA)) {
+                processosProntos.remove(processo);
+                tabelaDeProcessos.remove(processo);
+            } else {
+                decodificador.decodifica(processo, instrucao);
+
+                if (processo.getEstadoProcesso() == BLOQUEADO) {
+                    processosBloqueados.add(processo);
+                    localQuantum = 0;
+                } else {
+                    processosProntos.add(processo);
+                    localQuantum--;
+                }
+            }
+
+            if ((numProcessosExecutados % 2) == 0) {
+                verificaBloqueados();
+            }
+        }
+    }
+
+    private void verificaBloqueados() {
+        List<BCP> processosParaExclusao = new ArrayList<>();
+        processosBloqueados.forEach(processo -> {
+            processo.decTempoDeEspera();
+            if (processo.getTempoDeEspera() == 0) {
+                processosProntos.add(processo);
+                processosParaExclusao.add(processo);
+            }
+        });
+
+        for (BCP processo : processosParaExclusao) {
+            processosBloqueados.remove(processo);
         }
     }
 }
